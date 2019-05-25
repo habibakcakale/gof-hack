e4f6e285827df404525bc2ba140997da8f67cc0b
Bahadır Can Yıldız <bahadir.yildiz@osf-commerce.com>
2019-05-26 02:01:07 +0300

    auth-login: - Login added on both login and register pages

--- src/client/src/pages/login.vue
+++ src/client/src/pages/login.vue
@@ -1,3 +1,35 @@
+<script>
+import service from "@/utils/service";
+import { mapActions } from "vuex";
+export default {
+  name: "Login",
+  layout: "public",
+  data() {
+    return {
+      loginForm: {}
+    };
+  },
+  methods: {
+    ...mapActions("auth", ["SET_TOKEN"]),
+    handleLogin() {
+      // todo validation
+
+      service
+        .post("user/login", this.loginForm)
+        .then(({ isSuccess, token }) => {
+          console.log("[login.vue] Request successful");
+          // once back-end completed, we can check here.
+          // todo redirect dashboard after login
+          if (isSuccess) {
+            this.SET_TOKEN(token);
+            this.$router.push("dashboard");
+          }
+        });
+    }
+  }
+};
+</script>
+
 <template>
   <v-content>
     <v-container fluid fill-height>
@@ -11,6 +43,7 @@
             <v-card-text>
               <v-form>
                 <v-text-field
+                  v-model="loginForm.username"
                   prepend-icon="fa-facebook"
                   name="login"
                   label="Login"
@@ -18,6 +51,7 @@
                 ></v-text-field>
                 <v-text-field
                   id="password"
+                  v-model="loginForm.password"
                   prepend-icon="fa-twitter"
                   name="password"
                   label="Password"
@@ -27,7 +61,7 @@
             </v-card-text>
             <v-card-actions>
               <v-spacer></v-spacer>
-              <v-btn color="primary">Login</v-btn>
+              <v-btn color="primary" @click="handleLogin">Login</v-btn>
             </v-card-actions>
           </v-card>
         </v-flex>
@@ -35,9 +69,3 @@
     </v-container>
   </v-content>
 </template>
-<script>
-export default {
-  name: "Login",
-  layout: "public"
-};
-</script>
