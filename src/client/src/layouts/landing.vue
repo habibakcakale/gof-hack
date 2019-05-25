3bb2cc86dbb34c6d8e5d360c2c450d8fce93ac86
Bahadır Can Yıldız <bahadir.yildiz@osf-commerce.com>
2019-05-25 19:22:06 +0300

    [landingpage] - Added landing layout

---
+++ src/client/src/layouts/landing.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <v-toolbar fixed app :color="`elevation-0`">
+      <v-title>JAPE</v-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn flat>Login</v-btn>
+        <v-btn flat>Register</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <router-view />
+  </div>
+</template>
