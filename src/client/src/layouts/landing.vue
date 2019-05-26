a7be36191763674a45ddb3d59189503b984a0d67
Berkan Cetinkaya <berkan.cetinkaya@osf-commerce.com>
2019-05-26 05:04:53 +0300

    feature:  - style landing page - added about page and style

--- src/client/src/layouts/landing.vue
+++ src/client/src/layouts/landing.vue
@@ -1,13 +1,15 @@
 <template>
   <div>
-    <v-toolbar fixed app :color="`elevation-0`">
-      <v-title>JAPE</v-title>
-      <v-spacer></v-spacer>
-      <v-toolbar-items class="hidden-sm-and-down">
-        <v-btn flat :to="`login`">Login</v-btn>
-        <v-btn flat :to="`register`">Register</v-btn>
-      </v-toolbar-items>
-    </v-toolbar>
+    <landingNav />
     <router-view />
   </div>
 </template>
+
+<script>
+import landingNav from "./partials/landing-nav";
+export default {
+  components: {
+    landingNav
+  }
+};
+</script>
---
+++ src/client/src/layouts/partials/landing-nav.vue
@@ -0,0 +1,50 @@
+<template>
+  <header class="land_header">
+    <nav class="land_header_nav">
+      <div class="land_header_nav_block">
+        <router-link class="land_header_nav__link" :to="`/`"
+          >Home Page</router-link
+        >
+      </div>
+      <div class="land_header_nav_block">
+        <router-link class="land_header_nav__link" :to="`/about-product`"
+          >About Us</router-link
+        >
+        <router-link class="land_header_nav__link" :to="`/login`"
+          >Login</router-link
+        >
+        <router-link class="land_header_nav__link" :to="`/register`"
+          >Register</router-link
+        >
+      </div>
+    </nav>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+.land_header {
+  border: none;
+  margin: 0;
+  padding: 30px 0 20px;
+  max-width: 1157px;
+  width: 100%;
+  margin: 0 auto;
+
+  &_nav {
+    display: flex;
+    justify-content: space-between;
+    &_block {
+      display: block;
+    }
+    &__link {
+      text-decoration: none;
+      color: #5f7d95;
+      margin-right: 2rem;
+      font-size: 1.3rem;
+      &:hover {
+        color: #1273eb;
+      }
+    }
+  }
+}
+</style>
